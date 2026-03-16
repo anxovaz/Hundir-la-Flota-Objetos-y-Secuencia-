d3e1ea25e35833c392937f8c15905121afd00992
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-03-16 14:22:01 +0100

    __init__ y colocar_nave() clase Tablero

--- tablero.py
+++ tablero.py
@@ -10,17 +10,43 @@ class Tablero:
 
         self.naves = []
 
+        #inicializacion de las naves
+        por1 = Nave("Enterprise", "portaaviones", 5)
+
+        fra1 = Nave("Bismarck", "fragata", 3)
+        fra2 = Nave("Prince of Wales", "fragata", 3)
+        fra3 = Nave("Graf Spee", "fragata", 3)
+
+        sub1 = Nave("U-47", "submarino", 1)
+        sub2 = Nave("U-96", "submarino", 1)
+        sub3 = Nave("U-505", "submarino", 1)
+        sub4 = Nave("U-534", "submarino", 1)
+
+        self.casillero = [
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, por1, por1, por1, por1, por1, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, fra1, None, None, None, None, None, None],
+            [None, None, None, fra1, None, None, sub1, None, None, None],
+            [None, None, None, fra1, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, fra2, fra2, fra2, None, None, sub3, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, fra3, fra3, fra3, None, sub4, None, None, None, sub2]
+        ]
+
+
+
     def colocar_nave(self, nave, x, y, orientacion):
         """
         Coloca una nave en el tablero en las coordenadas especificadas
         """
 
-        nave = []
-        nave.append(nave)
-        nave.append(x)
-        nave.append(y)
-        nave.append(orientacion)
-        self.naves.append(nave)
+        for index, value in enumerate(self.casillero):
+            if index == y:
+                for i in range(0,nave.tamano):
+                    value[x] = nave
+                    x+=1
 
     def comprobar_impacto(self, x, y):
         """
