f33ffd86d12ed417a89ea27bd3efc3df475e91a2
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-03-17 13:58:49 +0100

    Fix class Nave  (#5)
    * metodo recibir_disparo de clase Nave
    
    * Agregar atributo tipo a nave + fix setters & getters

--- nave.py
+++ nave.py
@@ -1,9 +1,22 @@
 # Clase que representa una nave en el juego
-class Nave:
-    def __init__(self, nombre, tamano):
-        self.__nombre = nombre
-        self.__tamano = tamano
+from unboundmodule import regional_log_stats
 
+
+class Nave:
+    def __init__(self, nombre, tipo, tamano):
+        self.nombre = nombre
+        self.tamano = tamano
+        self.tipo = tipo
+
+    @property
+    def tipo(self):
+        return self.__tipo
+    @tipo.setter
+    def tipo(self, tipo):
+        if isinstance(tipo, str):
+            self.__tipo = tipo
+        else:
+            raise TypeError("Tipo tiene que ser string")
     @property
     def nombre(self):
         return self.__nombre
@@ -35,5 +48,13 @@ class Nave:
     def recibir_disparo(self):
         """
         Procesa el impacto en la nave
+
+        Como la vida es igual al tamanho y en esta clase no hay ningun atributo que haga referencia a la posicion en el
+        tablero, ya que esta en __init__ de Tablero en una lista, simplemente se le resta 1 a self.tamano
+
         """
-        return ""
+        self.tamano -= 1
+        if self.tamano < 0:
+            raise ValueError(f"Error no esperado, el atributo tamano es negativo. self.tamano -> {self.tamano}")
+        else:
+            return True #confirmación de que se ha quitado 1 vida (tamano) correctamente
